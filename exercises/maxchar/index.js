729fe4231b54a9c71f16f3f88f36632383846c68
hitvardhansolanki <43982595+hitvardhasolanki@users.noreply.github.com>
2019-05-19 12:52:56 +0530

    finding max characters in string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let strMap = {};
+  let max = 0;
+  let maxChar = '';
+  for (let char of str) {
+    strMap[char] = strMap[char] + 1 || 1;
+  }
+
+  for (let char in strMap) {
+    if (strMap[char] > max) {
+      max = strMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,15 +9,15 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-  let reversed = parseInt(
-    n
-      .toString()
-      .split('')
-      .reverse()
-      .join('')
+  return (
+    parseInt(
+      n
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+    ) * Math.sign(n)
   );
-
-  return reversed * Math.sign(n);
 }
 
 module.exports = reverseInt;
