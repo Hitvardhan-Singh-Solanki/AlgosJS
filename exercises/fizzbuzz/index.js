c4e35a9c2d8e5ec12efe2bd401bd9cb27de837f4
hitvardhansolanki <43982595+hitvardhasolanki@users.noreply.github.com>
2019-05-19 13:05:29 +0530

    fizzbuzz solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,20 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (var i = 1; i <= n; i++) {
+    console.log(fizzBuzzHelper(i));
+  }
+}
+
+function fizzBuzzHelper(num) {
+  if (num % 15 === 0) {
+    return 'fizzbuzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else return num;
+}
 
 module.exports = fizzBuzz;
