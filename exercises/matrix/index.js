57b6672ebd637568408f84bc703fcab1549fcc26
Hitvardhan Singh Solanki <hitvardhan19@gmail.com>
2019-12-19 23:25:44 +0530

    tress and matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,44 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = [];
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+  let counter = 1,
+    startColumn = 0,
+    endColumn = n - 1,
+    startRow = 0,
+    endRow = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // responsible for the TOP row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+    // responsible for RIGHT COLUMN
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+    // BOTTOM ROW;
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+    // LEFT COLUMN
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+  return results;
+}
 
 module.exports = matrix;
--- exercises/matrix/test.js
+++ exercises/matrix/test.js
@@ -7,6 +7,7 @@ test('matrix is a function', () => {
 test('matrix produces a 2x2 array', () => {
   const m = matrix(2);
   expect(m.length).toEqual(2);
+
   expect(m[0]).toEqual([1, 2]);
   expect(m[1]).toEqual([4, 3]);
 });
